4a390cfbff79ae6f1ef817d54a40d0d3f539879a
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 17:22:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,28 @@
-def using_concat
-
-end
-def using_insert
-#This method takes in two parameters, an Array and a new element to be added to the array. it uses the .insert method to add the new element to the 4th index of the array.
-
-
+def using_concat(arg , ar)
+  arg.concat(ar)
 end
 
-def using_uniq
-  #This method takes in a parameter of an Array and uses the .uniq method to remove any duplicate items.
-
-end 
-def using_flatten
+def using_insert(a, e)
+  #This method takes in two parameters, an Array and a new element to be added to the array. it uses the .insert method to add the new element to the 4th index of the array.
   
+  a.insert(e)
+end
+
+def using_uniq(a)
+  #This method takes in a parameter of an Array and uses the .uniq method to remove any duplicate items.
+  a.uniq
+end 
+def using_flatten(a)
+  a.flatten
 #This method takes in a parameter of an Array that contains other arrays and uses the .flatten method to return an array of strings.
 end
 
-def using_delete
+def using_delete(a,s)
+  a.delete(s)
 #This method takes in two parameters, an Array and a String, and uses the .delete method to remove any items from the array that are equal to that string.
 end
 
-using_delete_at
-This method takes in two parameters, an Array and an Integer and uses the .delete_at method to delete the element at the index of the array that is equal to the provided integer.
+def using_delete_at(a,i)
+  a.delete_at(i)
+#This method takes in two parameters, an Array and an Integer and uses the .delete_at method to delete the element at the index of the array that is equal to the provided integer.
+end 
