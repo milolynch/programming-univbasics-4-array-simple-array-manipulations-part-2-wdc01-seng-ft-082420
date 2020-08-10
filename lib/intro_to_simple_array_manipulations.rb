46fd8d8eab2e4357a055018db0f3176f8031b365
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 17:15:35 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_concat
+
+end
+using_insert
+#This method takes in two parameters, an Array and a new element to be added to the array. it uses the .insert method to add the new element to the 4th index of the array.
+end
+
+using_uniq
+This method takes in a parameter of an Array and uses the .uniq method to remove any duplicate items.
+
+using_flatten
+This method takes in a parameter of an Array that contains other arrays and uses the .flatten method to return an array of strings.
+
+using_delete
+This method takes in two parameters, an Array and a String, and uses the .delete method to remove any items from the array that are equal to that string.
+
+using_delete_at
+This method takes in two parameters, an Array and an Integer and uses the .delete_at method to delete the element at the index of the array that is equal to the provided integer.
